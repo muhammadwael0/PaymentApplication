ee28aff7cce74ae5201fa99d2897ab9fdf2bdcb2
Muhammad Wael <muhammad.wael2040@gmail.com>
2024-09-09 10:39:45 +0300

    adding Server files

---
+++ payment_system/server/server.c
@@ -0,0 +1,239 @@
+#include "server.h"
+
+static struct Node *list = NULL;
+
+EN_transaction_t receiveTransactionData(ST_transaction_t *transactionData)
+{
+    EN_transaction_t transactionError = APPROVED;
+    struct accountData accountData;
+    read_data_from_cards(&list);
+    if (isValidAccount(&transactionData->cardHolderData, &accountData) == SERVER_OK)
+    {
+        ST_accountsDB_t accountsDb;
+        accountsDb.balance = accountData.balance;
+        accountsDb.state = (!strcmp(accountData.state, "RUNNING") ? RUNNING : BLOCKED);
+        strcpy(accountsDb.primaryAccountNumber, accountData.primaryAccountNumber);
+        if (isBlockedAccount(&accountsDb) == SERVER_OK)
+        {
+            if (isAmountAvailable(&transactionData->terminalData, &accountsDb) == SERVER_OK)
+            {
+                accountData.balance -= transactionData->terminalData.transAmount;
+                update_node(list, &accountData);
+            }
+            else
+            {
+                transactionError = DECLINED_INSUFFICIENT_FUND;
+            }
+        }
+        else
+        {
+            transactionError = DECLINED_STOLEN_CARD;
+        }
+    }
+    else
+    {
+        transactionError = FRAUD_CARD;
+    }
+    if (transactionError == APPROVED)
+    {
+        write_data_to_cards(list);
+        saveTransaction(transactionData);
+    }
+    else
+    {
+        transactionError = INTERNAL_SERVER_ERROR;
+    }
+
+    free_memory(list);
+    return transactionError;
+}
+
+EN_serverError_t isValidAccount(ST_cardData_t *cardData, struct accountData *accountRef)
+{
+    EN_serverError_t serverError = SERVER_OK;
+    bool found = false;
+    struct Node *tempNode = list;
+    if (list == NULL || cardData == NULL || accountRef == NULL)
+    {
+        serverError = ACCOUNT_NOT_FOUND;
+    }
+    else
+    {
+        while (tempNode != NULL)
+        {
+            if (!strcmp(cardData->primaryAccountNumber, tempNode->NodeData.primaryAccountNumber))
+            {
+                found = true;
+                *accountRef = tempNode->NodeData;
+                break;
+            }
+            else
+            {
+                tempNode = tempNode->NodeLink;
+            }
+
+        }
+    }
+    if (found == true)
+    {
+        /* do nothing */
+    }
+    else
+    {
+        serverError = ACCOUNT_NOT_FOUND;
+    }
+    return serverError;
+}
+
+EN_serverError_t isBlockedAccount(ST_accountsDB_t *accountReference)
+{
+    EN_serverError_t serverError = SERVER_OK;
+    if (accountReference != NULL)
+    {
+        if (accountReference->state == RUNNING)
+        {
+            /* do nothing */
+        }
+        else if (accountReference->state == BLOCKED)
+        {
+            serverError = BLOCKED_ACCOUNT;
+        }
+    }
+    else
+    {
+        /* do nothing */
+    }
+    return serverError;
+}
+
+EN_serverError_t isAmountAvailable(ST_terminalData_t *terminalData, ST_accountsDB_t *accountReference)
+{
+    EN_serverError_t serverError = SERVER_OK;
+    if (accountReference == NULL || terminalData == NULL)
+    {
+        /* do nothing */
+    }
+    else
+    {
+        if (terminalData->transAmount <= accountReference->balance)
+        {
+            /* do nothing */
+        }
+        else
+        {
+             serverError = LOW_BALANCE;
+        }
+    }
+    return serverError;
+}
+
+EN_serverError_t saveTransaction(ST_transaction_t *transactionData)
+{
+    EN_serverError_t serverError = SERVER_OK;
+    QueueStatus_t status = QUEUE_NOK;
+    struct transactionData queue_obj, *ptr = NULL;
+    EN_fileError_t fileError = FILE_OK;
+
+    strcpy(queue_obj.transactionDate, transactionData->terminalData.transactionDate);
+    queue_obj.transactionAmount = transactionData->terminalData.transAmount;
+    strcpy(queue_obj.transactionState, (transactionData->transState == RUNNING ? "RUNNING" : "BLOCKED"));
+    queue_obj.terminalMaxAmount = transactionData->terminalData.maxTransAmount;
+    strcpy(queue_obj.cardHolderName, transactionData->cardHolderData.cardHolderName);
+    strcpy(queue_obj.PAN, transactionData->cardHolderData.primaryAccountNumber);
+    strcpy(queue_obj.EXP_DATE, transactionData->cardHolderData.cardExpirationDate);
+
+    Queue_t *queue = CreateQueue(10000, &status);
+    if (status == QUEUE_OK)
+    {
+        fileError = read_data_from_transaction(queue);
+        if (fileError == FILE_OK)
+        {
+            status = EnqueueElement(queue, &queue_obj);
+            if (status == QUEUE_OK)
+            {
+                ptr = QueueRear(queue, &status);
+                if (ptr != NULL)
+                {
+                    queue_obj.transactionSequenceNumber = ptr->transactionSequenceNumber + 1;
+                }
+                else
+                {
+                    queue_obj.transactionSequenceNumber = 56740000;
+                }
+                fileError = write_data_to_transaction(queue);
+            }
+            else
+            {
+                serverError = SAVING_FAILED;
+            }
+        }
+        else
+        {
+            serverError = SAVING_FAILED;
+        }
+    }
+    else
+    {
+        serverError = SAVING_FAILED;
+    }
+    if (fileError != FILE_OK)
+    {
+        serverError = SAVING_FAILED;
+    }
+    else
+    {
+        /* do nothing */
+    }
+    status = DestroyQueue(queue);
+    return serverError;
+}
+
+void listSavedTransaction(void)
+{
+    Queue_t *queue = NULL;
+    EN_fileError_t fileError = FILE_OK;
+    QueueStatus_t status = QUEUE_NOK;
+    struct transactionData *transactionData;
+
+    queue = CreateQueue(10000, &status);
+    if (status == QUEUE_OK)
+    {
+        fileError = read_data_from_transaction(queue);
+        if (fileError == FILE_OK)
+        {
+            while (status == QUEUE_OK)
+            {
+                transactionData = DequeueElement(queue, &status);
+                if (status == QUEUE_OK && transactionData != NULL)
+                {
+                    printf("########################################################\n");
+                    printf("Transaction Sequence Number: %i\n", transactionData->transactionSequenceNumber);
+                    printf("Transaction Date: %s\n", transactionData->transactionDate);
+                    printf("Transaction Amount: %.2f $\n", transactionData->transactionAmount);
+                    printf("Transaction State: %s\n", transactionData->transactionState);
+                    printf("Terminal Max Amount: %.2f $\n", transactionData->terminalMaxAmount);
+                    printf("Card Holder Name: %s\n", transactionData->cardHolderName);
+                    printf("PAN: %s\n", transactionData->PAN);
+                    printf("Card Expiration Date: %s\n", transactionData->EXP_DATE);
+                    printf("########################################################\n");
+                }
+                else
+                {
+                    /* do nothing */
+                }
+            }
+
+        }
+        else
+        {
+            /* do nothing */
+        }
+
+    }
+    else
+    {
+        /* do nothing */
+    }
+    status = DestroyQueue(queue);
+
+}
---
+++ payment_system/server/server.h
@@ -0,0 +1,53 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+
+#include "../../libs/std_types.h"
+#include "../card/card.h"
+#include "../terminal/terminal.h"
+#include "../../data_base/file_handling/file_handling.h"
+
+
+typedef enum EN_transaction_t {
+    APPROVED, DECLINED_INSUFFICIENT_FUND, DECLINED_STOLEN_CARD,
+    FRAUD_CARD, INTERNAL_SERVER_ERROR
+} EN_transaction_t;
+
+typedef struct ST_transaction_t {
+    ST_cardData_t cardHolderData;
+    ST_terminalData_t terminalData;
+    EN_transaction_t transState;
+    uint32 transactionSequenceNumber;
+} ST_transaction_t;
+
+typedef enum EN_serverError_t {
+    SERVER_OK, SAVING_FAILED, TRANSACTION_NOT_FOUND, ACCOUNT_NOT_FOUND,
+    LOW_BALANCE, BLOCKED_ACCOUNT
+} EN_serverError_t;
+
+typedef enum EN_accountState_t {
+    RUNNING,
+    BLOCKED
+} EN_accountState_t;
+
+typedef struct ST_accountsDB_t {
+    float balance;
+    EN_accountState_t state;
+    uint8 primaryAccountNumber[20];
+} ST_accountsDB_t;
+
+
+EN_transaction_t receiveTransactionData(ST_transaction_t *transactionData);
+
+EN_serverError_t isValidAccount(ST_cardData_t *cardData, struct accountData *accountRef);
+
+EN_serverError_t isBlockedAccount(ST_accountsDB_t *accountReference);
+
+EN_serverError_t isAmountAvailable(ST_terminalData_t *terminalData, ST_accountsDB_t *accountReference);
+
+EN_serverError_t saveTransaction(ST_transaction_t *transactionData);
+
+void listSavedTransaction(void);
+
+
+#endif

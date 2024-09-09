b60ee3e854f46ee037d7c2e9ddf3516ad27ae94d
Muhammad Wael <muhammad.wael2040@gmail.com>
2024-09-09 18:23:33 +0300

    modifying STD LIB

--- libs/std_types.h
+++ libs/std_types.h
@@ -5,12 +5,20 @@
 #define true 1
 #define false 0
 
+#define RESET  "\033[0m"
+#define RED    "\033[1;31m"
+#define BLUE   "\033[0;34m"
+#define YELLOW "\033[1;33m"
+#define GREEN  "\033[0;32m"
+
 typedef signed int sint32;
 typedef signed short sint16;
 typedef signed char sint8;
 typedef unsigned int uint32;
 typedef unsigned short uint16;
 typedef unsigned char uint8;
+
+
 
 
 #endif
